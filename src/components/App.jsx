00ead6a82143e79c308cfbdf8b1bffdfae0798df
allan <allan@lagann.charter.com>
2016-06-14 23:25:03 -0700

    Refacotred Results component by separting Tally into a separate component and added unit tests for Results component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('Trainspotting', '28 Days Later');
 const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
