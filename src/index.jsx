78606f07534647c9435d131bab9c625e6b887cce
allan <allan@lagann.charter.com>
2016-06-14 21:54:01 -0700

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
+import Voting from './components/Voting';
+import Results from './components/Results';
+
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
+
+ReactDOM.render(
+  <Router history={hashHistory}>{routes}</Router>,
+  document.getElementById('app')
+);
