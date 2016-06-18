be27d19ad3447402601f31b7718ff2f27fb66c2a
allan <allan@lagann.charter.com>
2016-06-18 10:56:50 -0700

    added actions into react components

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 import Tally from './Tally';
 
-export default React.createClass({
+export const Results = React.createClass({
     mixins: [PureRenderMixin],
     render: function() {
       return this.props.winner ?
@@ -20,3 +21,13 @@ export default React.createClass({
     </div>;
     }
 });
+
+function mapStateToProps(state) {
+    return {
+        pair: state.getIn(['vote', 'pair']),
+        tally: state.getIn(['vote', 'tally']),
+        winner: state.get('winner')
+    }
+}
+
+export const ResultsContainer = connect(mapStateToProps)(Results);
